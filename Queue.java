4d2e75ab6df46dda5eb6a7f8ccc264c9092b99cd
shivamj2603 <shivamj2603@gmail.com>
2020-10-14 07:45:41 +0530

    Added:Implementation of enque function

---
+++ Queue.java
@@ -0,0 +1,35 @@
+package stackandqueue;
+import com.java.datastructures.*;
+
+public class Queue<E> {
+	public LinkedList<E> linkedlist;
+	public Queue() {
+		linkedlist = new LinkedList<E>();
+	}
+	/**
+	 * Usecase 3
+	 * Function enqueues element on to the queue
+	 * @param node
+	 */
+	public void enque(INode<E> node) {
+		this.linkedlist.append(node);	
+	}
+	/**
+	 * Function prints the queue
+	 * 
+	 */
+	private void printQueue() {
+		this.linkedlist.printNodes();
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		Queue<Integer> queue = new Queue<Integer>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		queue.enque(firstNode);
+		queue.enque(secondNode);
+		queue.enque(thirdNode);
+		queue.printQueue();
+	}
+}
